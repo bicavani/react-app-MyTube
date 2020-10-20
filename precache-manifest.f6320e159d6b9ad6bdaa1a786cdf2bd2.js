906d7d756bd5999d290b79aaadc0808609342181
Namlongcomputer <hoandm91@gmail.com>
2020-10-20 18:06:01 +0700

    Updates

---
+++ precache-manifest.f6320e159d6b9ad6bdaa1a786cdf2bd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb191021ac967f9071b009526cd9786",
+    "url": "/react-app-MyTube/index.html"
+  },
+  {
+    "revision": "3721d8c7cd9e2e5b2efc",
+    "url": "/react-app-MyTube/static/js/2.e13e5e76.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/react-app-MyTube/static/js/2.e13e5e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15df0da076a74b67ac36",
+    "url": "/react-app-MyTube/static/js/main.df3b75d3.chunk.js"
+  },
+  {
+    "revision": "34764b3d704f1c6f08f2",
+    "url": "/react-app-MyTube/static/js/runtime-main.e008992c.js"
+  }
+]);
